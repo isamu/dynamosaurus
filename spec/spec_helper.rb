1f286623aad9034e433eda9f02e6aaf013e8e3b6
Isamu Arimoto <isamu@to-kyo.to>
2018-11-20 22:05:39 +0900

    config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,14 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 require 'dynamosaurus'
 require 'testmodel'
 require 'aws-sdk'
+
+ENV['DYNAMODB_SUFFIX'] = "_local"
+
+Aws.config = {
+  :endpoint => "http://localhost:8000",
+  :region => 'local_test',
+  :access_key_id => "test",
+  :secret_access_key => "test",
+}
